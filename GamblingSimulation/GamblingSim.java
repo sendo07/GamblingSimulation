32f10374cf0338cfdbb80ffbd23425aa1c72320b
sendo07 <loakesh.i@gmail.com>
2020-07-30 13:59:00 +0530

    added: gamble until 50% stake of win or loss is met

--- GamblingSimulation/GamblingSim.java
+++ GamblingSimulation/GamblingSim.java
@@ -7,21 +7,30 @@ public class GamblingSim {
   //Declaring initial Variables
   private static final int moneyAtStart=100;
   private static final int betAmt=1;
-  private static int gamblingMoney=moneyAtStart;
-  
+  private static int gamblingMoney = moneyAtStart;
+
   public static void main(String[] args) {
-    gamble();
+    gambler();
   }
 
-  //Function for a Single Gamble
+  public static void gambler() {
+    loop1: while (true) {
+      gamble();
+      if (gamblingMoney == 50 || gamblingMoney == 150) {
+        break loop1;
+      }
+    }
+  }
+
+  // Function for a Single Gamble
   public static void gamble() {
 
     Random r1 = new Random();
     boolean betCall = r1.nextBoolean();
-    
+
     if (betCall == true) {
-      //bet won
-      gamblingMoney+=betAmt;
+      // bet won
+      gamblingMoney += betAmt;
       System.out.println("Won: " +" "+ gamblingMoney+" " + moneyAtStart);
     } else if (betCall ==false) {
       //bet lost
