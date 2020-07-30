bf68d6e9ec879c57a6d2f119dba9072a42bf2347
sendo07 <loakesh.i@gmail.com>
2020-07-30 14:46:03 +0530

    display: Profit & Loss at day20

--- GamblingSimulation/GamblingSim.java
+++ GamblingSimulation/GamblingSim.java
@@ -8,18 +8,38 @@ public class GamblingSim {
   private static final int moneyAtStart=100;
   private static final int betAmt=1;
   private static int gamblingMoney = moneyAtStart;
+  private static int dayBetCount=0;
+  private static int profitAmt=0;
+  private static int lossAmt=0;
+
 
   public static void main(String[] args) {
-    gambler();
+    monthGamble();
   }
 
-  public static void gambler() {
+  public static void monthGamble() {
+    int i=1;
+    loop2: while (i<=30) {
+      singleDayGamble();
+      if (i==20) {
+        System.out.println("Total Profit in 20 days: "+profitAmt);
+        System.out.println("Total loss in 20 days: "+lossAmt);
+        break loop2;
+      }
+      i++;
+    }
+  }
+
+
+  public static int singleDayGamble() {
     loop1: while (true) {
       gamble();
+      dayBetCount++;
       if (gamblingMoney == 50 || gamblingMoney == 150) {
         break loop1;
       }
     }
+    return dayBetCount;
   }
 
   // Function for a Single Gamble
@@ -30,10 +50,12 @@ public class GamblingSim {
 
     if (betCall == true) {
       // bet won
+      profitAmt++;
       gamblingMoney += betAmt;
       System.out.println("Won: " +" "+ gamblingMoney+" " + moneyAtStart);
     } else if (betCall ==false) {
       //bet lost
+      lossAmt++;
       gamblingMoney-=betAmt;
       System.out.println("Lost: " +" "+ gamblingMoney+" " + moneyAtStart);
     }
