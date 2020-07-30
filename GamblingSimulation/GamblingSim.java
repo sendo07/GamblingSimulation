48c28f491b9fb4c4ecfb14ee006636668de0f5ca
sendo07 <loakesh.i@gmail.com>
2020-07-30 13:33:21 +0530

    Declare: GamblingAmt & BetAmt

---
+++ GamblingSimulation/GamblingSim.java
@@ -0,0 +1,13 @@
+package GamblingSimulation;
+
+public class GamblingSim {
+
+  //Declaring initial Variables
+  private static final int moneyAtStart=100;
+  private static final int betAmt=1;
+  private static int gamblingMoney=moneyAtStart;
+  
+  public static void main(String[] args) {
+    
+  }
+}
