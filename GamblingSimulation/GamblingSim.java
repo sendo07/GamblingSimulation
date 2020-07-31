7a0bcddf45793e1be03f5875accf517f6008aa90
sendo07 <loakesh.i@gmail.com>
2020-07-31 12:06:32 +0530

    Displaying Total Profit & Loss for everyday after day20

--- GamblingSimulation/GamblingSim.java
+++ GamblingSimulation/GamblingSim.java
@@ -8,15 +8,18 @@ public class GamblingSim {
   private static final int stake = 100;
   private static final int betAmt = 1;
   private static int gamblingMoney = stake;
-  private static int dayBetCount = 0;
   private static int profitAmt = 0;
   private static int lossAmt = 0;
+  private static int dayBetCount = 0;
+  private static int winCounter = 0;
+  private static int lossCounter = 0;
   private static double stakePercent = 0.50;
 
   public static void main(String[] args) {
     monthGamble();
   }
 
+  //Method for Gambling for a Month(30days)
   public static void monthGamble() {
     int i=1;
     while (i<=30) {
@@ -28,21 +31,27 @@ public class GamblingSim {
     }
   }
 
-
+  // Method for Gambling per Day
   public static int singleDayGamble() {
-    int winningStake = (int) (stake + (stakePercent*stake));
-    int losingStake = (int) (stake - (stakePercent*stake));
+    int stakeValue = (int) (stakePercent*stake);
+    int winningStake = (int) (stake + stakeValue);
+    int losingStake = (int) (stake - stakeValue);
     loop1: while (true) {
       gamble();
       dayBetCount++;
       if (gamblingMoney == losingStake || gamblingMoney == winningStake) {
+        if (gamblingMoney == losingStake) {
+          lossAmt+=stakeValue;
+        } else if (gamblingMoney == winningStake) {
+          profitAmt+=stakeValue;
+        }
         break loop1;
       }
     }
     return dayBetCount;
   }
 
-  // Function for a Single Gamble
+  // Method for a Single Gamble
   public static void gamble() {
 
     Random r1 = new Random();
@@ -50,12 +59,12 @@ public class GamblingSim {
 
     if (betCall == true) {
       // bet won
-      profitAmt++;
+      winCounter++;
       gamblingMoney += betAmt;
       //System.out.println("Won: " +" "+ gamblingMoney+" " + stake);
-    } else if (betCall ==false) {
+    } else if (betCall == false) {
       //bet lost
-      lossAmt++;
+      lossCounter++;
       gamblingMoney-=betAmt;
       //System.out.println("Lost: " +" "+ gamblingMoney+" " + stake);
     }
