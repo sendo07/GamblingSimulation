bd66f3038163c53a73b25c16a6cc574f28296217
sendo07 <loakesh.i@gmail.com>
2020-08-01 07:09:22 +0530

    refactor: constants & names

--- GamblingSimulation/GamblingSim.java
+++ GamblingSimulation/GamblingSim.java
@@ -10,14 +10,16 @@ import java.util.Arrays;
 
 public class GamblingSim {
 
-  //Declaring initial Variables
-  private static final int Stake = 100;
-  private static final int BetAmt = 1;
-  private static int gamblingMoney = Stake;
+  // Declaring Constants
+  private static final int STAKE = 100;
+  private static final int BET_AMT = 1;
+  private static final double STAKE_PERCENT = 0.50;
+
+  // Declaring Variables
+  private static int gamblingMoney = STAKE;
   private static int profitAmt = 0;
   private static int lossAmt = 0;
   private static int dayBetCount = 0;
-  private static double StakePercent = 0.50;
   private static int flag2;
   private static String flag1;
   private static LinkedHashMap<String,String> resultsMonth = new LinkedHashMap<>();
@@ -29,12 +31,12 @@ public class GamblingSim {
   }
 
   public static void welcomeScreen() {
-    System.out.println("\nWelcome to Gambling Simulation!\nStarting bets...");
+    System.out.println("\nWelcome to Gambling Simulation!\n\nStarting Stats: \n1. Stake = $100\n2. Bet Amount = $1\n3. Stake Percent =  50%\n\n\nStarting bets...");
   }
 
   public static void results() {
     
-    System.out.println("\n\n========================");
+    System.out.println("\n\n__________________________");
     System.out.println("\n\nDaywise Results: \n");
 
     for (String key: resultsMonth.keySet()) {
@@ -46,16 +48,16 @@ public class GamblingSim {
   //Method for Gambling for a Month(30days)
   public static void monthGamble() {
     int i=1;
-    System.out.println("\n\n========================");
+    System.out.println("\n\n__________________________");
     System.out.println("\n\nProfit & Loss Details (Day21 onwards)");
     
     while (i<=30) {
-      ArrayList<Integer> x = singleDayGamble();
+      ArrayList<Integer> arrTemp = singleDayGamble();
 
-      if (x.get(2) == 1) {
-        resultsMonth.put("Day"+i, "Won" +" by "+ x.get(0) +" bets");
-      } else if (x.get(2) == 0) {
-        resultsMonth.put("Day"+i, "Lost" +" by "+ x.get(1) +" bets");
+      if (arrTemp.get(2) == 1) {
+        resultsMonth.put("Day"+i, "Won" +" by "+ arrTemp.get(0) +" bets");
+      } else if (arrTemp.get(2) == 0) {
+        resultsMonth.put("Day"+i, "Lost" +" by "+ arrTemp.get(1) +" bets");
       }
       
       if (i>20) {
@@ -68,9 +70,9 @@ public class GamblingSim {
   // Method for Gambling per Day
   public static ArrayList<Integer> singleDayGamble() {
 
-    int StakeValue = (int) (StakePercent*Stake);
-    int winningStake = (int) (Stake + StakeValue);
-    int losingStake = (int) (Stake - StakeValue);
+    int STAKEValue = (int) (STAKE_PERCENT*STAKE);
+    int winningSTAKE = (int) (STAKE + STAKEValue);
+    int losingSTAKE = (int) (STAKE - STAKEValue);
 
     int winCounter = 0;
     int lossCounter = 0;
@@ -84,13 +86,13 @@ public class GamblingSim {
       }else if ( y == "Lost") {
         lossCounter++;
       }
-      if (gamblingMoney == losingStake || gamblingMoney == winningStake) {
-        if (gamblingMoney == losingStake) {
-          lossAmt+=StakeValue;
+      if (gamblingMoney == losingSTAKE || gamblingMoney == winningSTAKE) {
+        if (gamblingMoney == losingSTAKE) {
+          lossAmt+=STAKEValue;
           flag2 = 0; // Day Lost
-        } else if (gamblingMoney == winningStake) {
-          profitAmt+=StakeValue;
-          flag2 = 1; //Day Won
+        } else if (gamblingMoney == winningSTAKE) {
+          profitAmt+=STAKEValue;
+          flag2 = 1; // Day Won
         }
         break loop1;
       }
@@ -110,11 +112,11 @@ public class GamblingSim {
     if (betCall == true) {
       // bet won
       flag1 = "Won";
-      gamblingMoney += BetAmt;
+      gamblingMoney += BET_AMT;
     } else if (betCall == false) {
       // bet lost
       flag1 = "Lost";
-      gamblingMoney -= BetAmt;
+      gamblingMoney -= BET_AMT;
     }
     return flag1;
   }
